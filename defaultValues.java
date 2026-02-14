b6180b4ecede9433e18812503ef70753db4b9837
Radhik5v3 <244g1a05v3@srit.ac.in>
2026-02-14 10:57:18 +0530

    Add files via upload

---
+++ defaultValues.java
@@ -0,0 +1,25 @@
+ class defaultValues {
+    byte b;
+    int i;
+    short s;
+    long l;
+    double d;
+    float f;
+    char c;
+    boolean t;
+    public static void main(String[] args) {
+        defaultValues obj=new defaultValues();
+        System.out.println("Default Values of the all primitive data types:");
+        System.out.println("byte"+obj.b);
+        System.out.println("int"+obj.i);
+        System.out.println("short"+obj.s);
+        System.out.println("long"+obj.l);
+        System.out.println("double"+obj.d);
+        System.out.println("float"+obj.f);
+        System.out.println("char"+obj.c);
+        System.out.println("boolean"+obj.t);
+    }
+
+  
+    
+}
