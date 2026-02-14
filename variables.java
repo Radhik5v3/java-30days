55f509796cd882322374d623d28f400dec809e51
Radhik5v3 <244g1a05v3@srit.ac.in>
2026-02-14 11:13:40 +0530

    Basics

---
+++ variables.java
@@ -0,0 +1,16 @@
+class variables{ 
+public static void main(String[] args) {
+int num1 = 2;
+int num2 = 3;
+System.out.println(num1+num2);
+
+
+
+}
+    
+
+
+
+
+
+}
---
+++ variables2.java
@@ -0,0 +1,10 @@
+ class variables2 {
+    public static void main(String[] args) {
+        int num1 = 100008465;
+        int num2 =1289748768;
+        int result = num1+num2;
+        System.out.println(result);
+        
+    }
+    
+}
