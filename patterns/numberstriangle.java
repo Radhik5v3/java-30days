feca2d4371b311a014237716eabb38d2be2354ee
Radhik5v3 <244g1a05v3@srit.ac.in>
2026-02-24 19:13:20 +0530

    Add files via upload

---
+++ patterns/numberstriangle.java
@@ -0,0 +1,12 @@
+package patterns;
+
+public class numberstriangle {
+    public static void main(String[] args) {
+        int n=15;
+        for(int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+        System.out.println();
+    }
+    
+}
