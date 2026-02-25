d4a21ff24d8c4ea27d50b7d1e6f119862e5499bb
Radhik5v3 <244g1a05v3@srit.ac.in>
2026-02-25 19:03:41 +0530

    methods-basics

---
+++ methods.java
@@ -0,0 +1,13 @@
+public class methods {
+    void sayHello(){
+        System.out.println("Hello Radhik");
+        
+    }
+    public static void main(String[] args) {
+        methods m=new methods();
+        m.sayHello();
+
+        
+    }
+    
+}
