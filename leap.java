06e4c8f8862cef26d1f4e1c2ae5d09f188f23b57
Radhik5v3 <244g1a05v3@srit.ac.in>
2026-02-27 20:45:35 +0530

    basic

---
+++ leap.java
@@ -0,0 +1,10 @@
+ class leap {
+    public static void main(String[] args) {
+        int n = 2024;
+        if (n% 4==0)
+            System.out.println("it is leap year");
+        else
+            System.out.println("it is not a leap year ");
+    }
+    
+}
